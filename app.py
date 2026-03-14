6e929a6c20c30587aa52ca4e48f7fb5e92691d44
Rafael <rafaelcamargo.gomes@outlook.com>
2026-03-14 18:22:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 print("⚡ Calculadora de Consumo de Energia ⚡")
 
+# Entrada de dados
 aparelho = input("Digite o nome do aparelho: ")
 potencia = float(input("Digite a potência do aparelho em watts (W): "))
 horas_dia = float(input("Digite o tempo médio de uso diário (horas): "))
 
+# Cálculo do consumo mensal em kWh
 consumo_mensal = (potencia * horas_dia * 30) / 1000
 
+# Cálculo do custo estimado
 valor_kwh = 0.75
 custo_estimado = consumo_mensal * valor_kwh
 
+# Resultado
 print("\n📊 Resultado")
 print("Aparelho:", aparelho)
-print("Consumo estimado:", round(consumo_mensal,2), "kWh/mês")
-print("Custo estimado: R$", round(custo_estimado,2))
+print("Consumo estimado:", round(consumo_mensal, 2), "kWh/mês")
+print("Custo estimado: R$", round(custo_estimado, 2))
